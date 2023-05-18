6d922d59d562ce628e155c7edf8abe32a9fc3b8e
iPresing <119415796+iPresing@users.noreply.github.com>
2023-05-18 03:58:31 +0200

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Définir la racine du système de fichiers à parcourir
+root_dir="/"
+
+# Parcourir récursivement tous les fichiers du système
+find "$root_dir" -type f -print0 | while IFS= read -r -d '' file; do
+    # Utiliser grep pour chercher la chaîne "404CTF" dans le fichier
+    # -l arrête la recherche après la première correspondance
+    # -q rend grep silencieux (pas de sortie standard)
+    if grep -lq "404CTF" "$file"; then
+        # Si la chaîne a été trouvée, afficher le contenu du fichier
+        echo "Found in: $file"
+        cat "$file"
+    fi
+done

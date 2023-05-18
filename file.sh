34de2a1490e2bc407e75322a3fb7826bd1e17f5f
iPresing <119415796+iPresing@users.noreply.github.com>
2023-05-18 04:02:46 +0200

    Update file.sh

--- file.sh
+++ file.sh
@@ -5,6 +5,11 @@ root_dir="/"
 
 # Parcourir récursivement tous les fichiers du système
 find "$root_dir" -type f -print0 | while IFS= read -r -d '' file; do
+    # Exclure le script lui-même
+    if [[ $file == *"/find_404CTF.sh" ]]; then
+        continue
+    fi
+    
     # Utiliser grep pour chercher la chaîne "404CTF" dans le fichier
     # -l arrête la recherche après la première correspondance
     # -q rend grep silencieux (pas de sortie standard)
